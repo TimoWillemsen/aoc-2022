dc6ef6d22e893163c8e4e9112a884af8a0fc9e38
Timo Willemsen <t.willemsen@quin.md>
2022-12-06 15:58:50 +0100

    marker

--- src/main/kotlin/Day06.kt
+++ src/main/kotlin/Day06.kt
@@ -1,14 +1,10 @@
-import java.time.Instant
-
 fun main() {
+    fun String.findMarker(markerSize: Int) = this
+        .windowed(markerSize, 1)
+        .map { it.toList().distinct().size }
+        .also { println(it.indexOf(markerSize) + markerSize) }
 
     loadFile("day06.txt")
-        .windowed(14, 1)
-        .map { it.toList().distinct().size }
-        .also { println(it.indexOf(14) + 14) }
-
-    loadFile("day06.txt")
-        .windowed(4, 1)
-        .map { it.toList().distinct().size }
-        .also { println(it.indexOf(4) +4) }
+        .apply { findMarker(4) }
+        .apply { findMarker(14) }
 }
